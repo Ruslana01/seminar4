60865d37663914635f4dd00cac521646635f7ce7
Руслана <shabaevaruslana@yandex.ru>
2022-11-12 23:06:07 +0300

    Задача 1 (№25)

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 Console.WriteLine("введите число A");
-int a = int.Parse(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число B");
-int b = int.Parse(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 int number = a;
 
 for (int i = 1; i < b; i++)
